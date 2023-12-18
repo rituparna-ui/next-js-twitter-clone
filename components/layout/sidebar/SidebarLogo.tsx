311ae13d03d8ba24df80428086d209ccbd788dff
Rituparna W <rituparnaw16@gmail.com>
2023-12-18 15:40:33 +0530

    Initial layout. Sidebar and followbar

---
+++ components/layout/sidebar/SidebarLogo.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import { BsTwitter } from 'react-icons/bs';
+
+const SidebarLogo = () => {
+  const router = useRouter();
+
+  return (
+    <div
+      onClick={() => router.push('/')}
+      className="
+        rounded-full
+        h-14
+        w-14
+        flex
+        items-center
+        justify-center
+        hover:bg-blue-300
+        hover:bg-opacity-10
+        cursor-pointer
+        transition
+      "
+    >
+      <BsTwitter
+        size={28}
+        color="white"
+      />
+    </div>
+  );
+};
+
+export default SidebarLogo;
